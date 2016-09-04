64e91546efb3d07566275c0d97c7ff7310afa775
Erwin Atuli <erwin@promotexter.com>
2016-09-04 22:40:46 +0800

    added readme and config comments

---
+++ README.md
@@ -0,0 +1,3 @@
+EC2 Scheduler
+=============
+A cron like utility for starting/stopping AWS EC2 instances
--- config/config-sample.js
+++ config/config-sample.js
@@ -1,14 +1,23 @@
 module.exports = {
+    // aws access key
     default_access_key: "",
+    // aws secret key
     default_secret_key: "",
     default_timezone: "Asia/Manila",
     default_region: "us-east-1",
+
+    // default settings can be overridden in the
+    // actual job
     scheduler: [
         {
             cron: "* * * * * *",
-            instances: ['xxxxxx'],
-            action: "start"
-            region: "ap-southeast-1"
+            instances: ['instace-id'],
+            //available actions are stop and start
+            action: "start",
+
+            access_key: "",
+            secret_key: "",
+            region: "ap-southeast-1",
         }
     ]
 };
